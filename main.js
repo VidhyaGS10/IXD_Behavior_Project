e0dcf17fb4af331c4f2abddeaa2f36d928c7f2dd
VidhyaGS <ganapavi@shc:\Users\VGS\Downloads\IXD\SEM_2\TYPOGRAPHY\student-web-template2022\index.htm>
2023-02-08 13:49:22 -0500

    working form

---
+++ main.js
@@ -0,0 +1,22 @@
+function main (){
+
+}
+
+
+function tax_percentage(){
+    return 'tax_percentage';
+}
+
+function checkForm() {
+    // console.log ('this works!');     
+    let amount = document.getElementById ('income').value;
+    let territory = document.getElementById('province').value;
+    let final = document.getElementById('result');
+    // let text = "Your final price is " + "Hurray!";
+    let text = "Your final price is " + "CA$" + amount + " " + territory;
+    result.innerHTML = text;
+}
+
+function budget () {
+
+}
